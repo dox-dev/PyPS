69a5e6fac635d32e52de51b95348d8f03909641b
Dox <doxofficial1235@gmail.com>
2022-03-21 12:03:58 +0100

    Initial Update

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from PyPS import print
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'PYPS',
+  packages = ['PYPS'],
+  version = '0.1',
+  license='MIT',
+  description = 'Python Pretty Strings. Use colors in your python strings.',
+  author = 'DoxDev',
+  author_email = 'hello@aestheticdream.cc',
+  url = 'https://github.com/dox-dev/PyPS',
+  download_url = 'https://github.com/dox-dev/PyPS/archive/refs/heads/main.zip',
+  keywords = ['Python', 'Print', 'Pretty Strings', 'PyPS'],
+  install_requires=[
+          'sys',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
